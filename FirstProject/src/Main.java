73dea8a2e4e0348aae06f9c29d00e7d898df3265
Avinoam Ruach <ruachavinoam@gmail.com>
2019-10-05 18:33:50 -0700

    first project if/else

---
+++ FirstProject/src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main (String[] args){
+
+        int age = 23;
+        for (age = 0; age <= 23; age++) {
+            if (age < 23){
+                continue;
+            }else {
+                System.out.print(age);
+            }
+        }
+
+        }
+
+    }
+
